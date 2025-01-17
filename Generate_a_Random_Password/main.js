6ed7df2916dcc9d878b376759d9c9747d77dc871
Anass Cheab <anassalien123@gmail.com>
2025-01-17 02:20:23 +0100

    New

---
+++ Generate_a_Random_Password/main.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const lenght =12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+=-,./;|{}[]~";
+
+const allChars = upperCase + lowercase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
